db93d1b42cd858b6176002f5a15a41cc96e338f0
Pugavkomm <slavapugavko2@gmail.com>
2024-12-20 02:17:55 -0300

    Move urls inside auth_api

--- server/auth_api/urls.py
+++ server/auth_api/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from auth_api import views
 
 urlpatterns = [
     path("sign-up/", views.SignUpView.as_view(), name="sign-up"),
     path("sign-in/", views.LogInView.as_view(), name="sign-in"),
+    path("token/", TokenObtainPairView.as_view(), name="token-obtain-pair"),
+    path("refresh/", TokenRefreshView.as_view(), name="token-refresh"),
 ]
