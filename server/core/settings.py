29e5f68f091c134c46d4993bde2f6c7ec3cbaa37
Pugavkomm <slavapugavko2@gmail.com>
2024-12-24 02:16:39 -0300

    Allow cors headers

--- server/core/settings.py
+++ server/core/settings.py
@@ -26,10 +26,14 @@ SECRET_KEY = "django-insecure-uggfjko(b99-03)!x4-+rwpdxy6bqxunkh#mw85yu$7vsde-th
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:4200",
+]
 
 # Application definition
 
 INSTALLED_APPS = [
+    "corsheaders",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -45,6 +49,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
